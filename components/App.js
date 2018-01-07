4f2ceb9ea37c842896d2f7f0a191204e6b8a0a9c
Adam Więcek <adamw28@gmail.com>
2018-01-07 19:28:53 +0100

    Update project

---
+++ components/App.js
@@ -0,0 +1,10 @@
+var App = React.createClass({
+  render: function() {
+    return (
+      <div {className: 'app'}>
+        React.createElement(ContactForm, {contact: contactForm}),
+        React.createElement(Contacts, {items: contacts}, {})
+      </div>
+    );
+  }
+});
